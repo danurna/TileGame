f71b8465d819fd676f9979ca02752ae82eb5987d
Daniel Witurna <daniel.witurna@gmail.com>
2014-07-19 16:30:09 +0200

    Initial Commit

---
+++ TileGame/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  TileGame
+//
+//  Created by Daniel Witurna on 19.07.14.
+//  Copyright Daniel Witurna 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
